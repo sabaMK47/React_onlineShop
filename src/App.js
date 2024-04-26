4dca2803ce1c4850222e8c20c23590b5dc0ac95e
maryam.khosro <118604201+sabaMK47@users.noreply.github.com>
2024-04-26 23:09:28 +0330

    routing pages and navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
-
+import React from "react";
+import {BrowserRouter , Routes , Route} from "react-router-dom";
+import Nav from "./components/Nav";
+import Basket from "./pages/Basket";
+import Shop from "./pages/Shop";
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+      <Nav/>
+      <Routes>
+        <Route path="/" element={<Shop/>}/>
+        <Route path="/basket" element={<Basket/>}/>
+      </Routes>
+      </BrowserRouter>
       
     </div>
   );
---
+++ src/components/Nav.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import "bootstrap/dist/css/bootstrap.min.css";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
+import {faBasketShopping} from "@fortawesome/free-solid-svg-icons"
+import {Link} from 'react-router-dom'
+function Nav() {
+  return (
+    <>
+    <div className='navbar navbar-expand-lg bg-danger'>
+        <div className='container'>
+            <a className='navbar-brand text-white'>my online shop</a>
+            <ul className='navbar-nav'>
+                <Link to="/" className='navbar-item text-white mx-5 link-offset-2 link-offset-3-hover'>Shop</Link>
+                <Link to="/Basket" className='navbar-item text-white fs-5'><FontAwesomeIcon icon={faBasketShopping}/></Link>
+
+            </ul>
+        </div>
+    </div>
+
+    </>
+  )
+}
+
+export default Nav
