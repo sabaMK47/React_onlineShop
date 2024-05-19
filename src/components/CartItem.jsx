ec9ad56bead47b57c266d627cfacc305597d7394
maryam.khosro <118604201+sabaMK47@users.noreply.github.com>
2024-05-19 22:55:28 +0330

    cart item design

---
+++ src/components/CartItem.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faTrashCan, faAdd, faMinus } from "@fortawesome/free-solid-svg-icons"
+import { useContext } from 'react';
+import { ShopContext } from '../data/ShopContext';
+
+export function CartItem(props) {
+    const { id, bookImage, bookName, price } = props.data;
+    const { cartItems, addToCart, removeFromCart } = useContext(ShopContext)
+    return (
+        <div>
+            <div className='row mx-auto d-flex m-4 w-50 border rounded' style={{ position: "relative" }}>
+                <div className='col-2 d-flex p-3'>
+                    <img src={bookImage} style={{ width: "150px", height: "200px" }} />
+                </div>
+                <div className='col-6 p-3 mx-5'>
+                    <h4>{bookName}</h4>
+                    <h4>{price}</h4>
+                    <div className='mt-5'>
+                        <button className='btn btn-danger p-1 mt-1 mt-1' onClick={() => addToCart(id)}><FontAwesomeIcon icon={faAdd} /></button>
+                        <span className='px-1'>{cartItems[id]}</span>
+                        <button className='btn btn-danger p-1 mt-1 mt-1' onClick={() => removeFromCart(id)}><FontAwesomeIcon icon={faMinus} /></button>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+    )
+}
+
--- src/pages/Basket.jsx
+++ src/pages/Basket.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { useContext } from 'react'
+import {CartItem} from '../components/CartItem'
+import { Products } from '../data/Products'
+import { ShopContext } from '../data/ShopContext'
+
 
 function Basket() {
+  const {cartItems}=useContext(ShopContext)
   return (
-    <div>Basket</div>
+    <div>
+      <h1 style={{marginLeft:"38%",padding:"30px"}}>your cart items</h1>
+      {Products.map((item)=>{
+        if(cartItems[item.id]!==0)
+        return <CartItem data={item}/>
+      })}
+    </div>
   )
 }
 
