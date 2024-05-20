5716f4845de112fa4bfeff032710a6b98bab5fdc
maryam.khosro <118604201+sabaMK47@users.noreply.github.com>
2024-05-20 15:10:29 +0330

    subtract button hided

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -3,11 +3,15 @@ import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
 import {faAdd , faMinus} from "@fortawesome/free-solid-svg-icons"
 import { useContext } from 'react'
 import {ShopContext} from '../data/ShopContext'
+import { Products } from '../data/Products'
+
+
 
 
 function Product(props) {
     const {id , bookImage , bookName , authorName , price ,quantity} = props.data;
     const {addToCart,removeFromCart , cartItems} =useContext(ShopContext)
+    const isincart = Products.some((item)=>(cartItems[item.id]!==0))
   return (
     <div className='col-2 my-4'>
     <div className='card h-100'>
@@ -19,8 +23,8 @@ function Product(props) {
             <div className='btns' style={{position:'absolute',bottom:'15px'}}>
             <button className='btn btn-danger p-1 mx-1 mt-1'>see details</button>
             <button className='btn btn-danger p-1 mt-1 mt-1' onClick={()=>addToCart(id)}><FontAwesomeIcon icon={faAdd}/></button>
-            <span className='px-1'>{cartItems[id]}</span>
-            <button className='btn btn-danger p-1 mt-1 mt-1' onClick={()=>removeFromCart(id)}><FontAwesomeIcon icon={faMinus}/></button>
+            {isincart &&<span className='px-1'>{cartItems[id]}</span>}
+            {isincart &&<button className='btn btn-danger p-1 mt-1 mt-1' onClick={()=>removeFromCart(id)}><FontAwesomeIcon icon={faMinus}/></button>}
             </div>
 
         </div>
